d4a5be838946068747783891c87033997bc486c0
Just R <remixingmagelang@gmail.com>
2024-08-17 01:36:19 +0700

    update modules/ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -6,7 +6,7 @@
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 
-from time
+import time
 
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
