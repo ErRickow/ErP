624ac1867d00996e0ca2664768e61c6a7a5e6698
Just R <remixingmagelang@gmail.com>
2024-08-17 01:17:07 +0700

    update modules/python.py

--- modules/python.py
+++ modules/python.py
@@ -42,7 +42,7 @@ async def user_exec(client: Client, message: Message):
     code = message.text.split(maxsplit=1)[1]
     stdout = StringIO()
 
-    await message.edit("<blockquote>Wet...</blockquote>")
+    await message.reply("<blockquote>Wet...</blockquote>")
 
     try:
         with redirect_stdout(stdout):
