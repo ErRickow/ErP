e4a27e7f290d95c497ba23af116d7e6651bb4598
Just R <remixingmagelang@gmail.com>
2024-08-17 00:50:12 +0700

    update utils/misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -40,7 +40,7 @@ try:
 except git.exc.InvalidGitRepositoryError:
     repo = git.Repo.init()
     origin = repo.create_remote(
-        "origin", "https://github.com/The-MoonTg-project/Moon-Userbot"
+        "origin", "https://github.com/ErRickow/ErP"
     )
     origin.fetch()
     repo.create_head("main", origin.refs.main)
