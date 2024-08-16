d0dcb4d147377a197635500a335b870fd038d1a9
Just R <remixingmagelang@gmail.com>
2024-08-17 02:31:56 +0700

    update modules/help.py

--- modules/help.py
+++ modules/help.py
@@ -1,15 +1,4 @@
-#  Moon-Userbot - telegram userbot
-#  Copyright (C) 2020-present Moon Userbot Organization
-#
-#  This program is free software: you can redistribute it and/or modify
 
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
